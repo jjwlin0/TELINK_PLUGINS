82410c9414d3680689f2cd11ec0db5bfbeaa1b82
jw.lin <jw.lin@telink-semi.com>
2017-05-16 09:44:19 +0800

    init project

---
+++ com.nanosic.tc32eclipse.core/src/com/nanosic/tc32eclipse/core/paths/TC32Path.java
@@ -0,0 +1,78 @@
+/******************************************************************************
+ * Copyright (c) 2009-2016 Telink Semiconductor Co., LTD.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * -----------------------------------------------------------------------------
+ * Module:
+ * Purpose:
+ * Reference :   
+ * $Id: DirectoryNotStrictVariableFieldEditor.java 851 20.1.08-07 19:37:00Z innot $
+ *******************************************************************************//******************************************************************************
+ * Copyright (c) 2009-2016 Telink Semiconductor Co., LTD.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * -----------------------------------------------------------------------------
+ * Module:
+ * Purpose:
+ * Reference : 
+ * $Id: TC32Path.java 851 21.0.38-07 19:37:00Z innot $
+ *     
+ *******************************************************************************/
+package com.nanosic.tc32eclipse.core.paths;
+
+public enum TC32Path {
+	// The compiler
+	TC32_GCC(true, "tc32-elf-gcc","Directory containing tc32-elf-* toolchain","tc32-elf-gcc"),
+
+	// Make
+	MAKE(true, "GNU make", "Directory containing 'make' executable", "make"),
+
+	// The Python for gdb
+ 	//TC32_PYTHON(true, "gdb python", "Directory containing python script", "python"), 
+
+	// PCDB executable
+	TC32_TOOLS(false, "tcdb", "Directory containing tools executable", "tcdb");
+
+
+	private boolean	fRequired;
+	private String	fName;
+	private String	fDescription;
+	private String	fTest;
+
+	/**
+	 * Default Enum constructor. Sets the internal fields according to the selected enum value.
+	 */
+	TC32Path(boolean required, String name, String description, String test) {
+		fRequired = required;
+		fName = name;
+		fDescription = description;
+		fTest = test;
+	}
+
+	public String getDescription() {
+		return fDescription;
+	}
+
+	public String getName() {
+		return fName;
+	}
+
+	public boolean isOptional() {
+		return !fRequired;
+	}
+
+	public String getTest() {
+		return fTest;
+	}
+
+	@Override
+	public String toString() {
+		return fName;
+	}
+}
